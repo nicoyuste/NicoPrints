dd00df3fd83208011b50b07c4839e11cd09488bb
Nicolas Yuste Tirados <nicolasy@spotify.com>
2025-08-26 23:55:42 +0200

    Fix problem with color name

--- src/colors.js
+++ src/colors.js
@@ -1,14 +1,14 @@
 export const COLORS = [
-  { value: 'B-PLA_Ba-Red', label: 'Rojo', hex: '#C12E1F', material: 'PLA' },
-  { value: 'B-PLA_Ba-Blue', label: 'Azul', hex: '#0A2989', material: 'PLA' },
-  { value: 'B-PLA_Ba-Yell', label: 'Amarillo', hex: '#F4EE2A', material: 'PLA' },
-  { value: 'B-PLA_Ba-Green', label: 'Amarillo', hex: '#00AE42', material: 'PLA' },
-  { value: 'B-PLA_Ma-Black', label: 'Negro mate', hex: '#000000', material: 'PLA' },
-  { value: 'B-PLA_Ma-White', label: 'Blanco mate', hex: '#FFFFFF', material: 'PLA' },
-  { value: 'B-PETG-Trans', label: 'Translucido', hex: '#A6A9AA', material: 'PETG' },
-  { value: 'PRU-PLA-GaBlack', label: 'Galaxy Black', hex: '#163114', material: 'PLA' },
-  { value: 'PRU-PLA-Orange', label: 'Prusa Orange', hex: '#FF7238', material: 'PLA' },
-  { value: 'PRU-PETG-Grey', label: 'Urban Grey', hex: '#BED2AF', material: 'PETG' },
+  { value: 'B_PLA_Ba_Red', label: 'Rojo', hex: '#C12E1F', material: 'PLA' },
+  { value: 'B_PLA_Ba_Blue', label: 'Azul', hex: '#0A2989', material: 'PLA' },
+  { value: 'B_PLA_Ba_Yell', label: 'Amarillo', hex: '#F4EE2A', material: 'PLA' },
+  { value: 'B_PLA_Ba_Green', label: 'Verde', hex: '#00AE42', material: 'PLA' },
+  { value: 'B_PLA_Ma_Black', label: 'Negro mate', hex: '#000000', material: 'PLA Mate' },
+  { value: 'B_PLA_Ma_White', label: 'Blanco mate', hex: '#FFFFFF', material: 'PLA Mate' },
+  { value: 'B_PETG_Trans', label: 'Translucido', hex: '#A6A9AA', material: 'PETG' },
+  { value: 'PRU_PLA_GaBlack', label: 'Galaxy Black', hex: '#163114', material: 'PLA' },
+  { value: 'PRU_PLA_Orange', label: 'Prusa Orange', hex: '#FF7238', material: 'PLA' },
+  { value: 'PRU_PETG_Grey', label: 'Urban Grey', hex: '#BED2AF', material: 'PETG' },
 ]
 
 
