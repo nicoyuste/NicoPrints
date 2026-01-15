dd324932f1ae6c36100b9b979ae52b8171d499d9
Nicolas Yuste Tirados <nicolasy@spotify.com>
2026-01-15 23:38:59 +0100

    Adding the Old Borders Cup

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,22 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    {
+      name: 'oldborderscup-dev-index-rewrite',
+      configureServer(server) {
+        server.middlewares.use((req, _res, next) => {
+          const url = req.url || ''
+          const [pathname, search] = url.split('?')
+          if (pathname === '/oldborderscup' || pathname === '/oldborderscup/') {
+            req.url = `/oldborderscup/index.html${search ? `?${search}` : ''}`
+          }
+          next()
+        })
+      },
+    },
+    react(),
+  ],
   // Custom domain deployment
   base: '/',
   resolve: {
